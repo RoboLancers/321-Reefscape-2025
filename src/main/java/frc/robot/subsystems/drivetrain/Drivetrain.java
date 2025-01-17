13252ff07f965804ea291b691f56fa2a1be7dca5
BananasAmIRite <jasony1230@gmail.com>
2025-01-17 15:32:49 -0500

    fix(documentation): added comments referring to code structure and custom logger guides

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -1,6 +1,10 @@
 /* (C) Robolancers 2025 */
 package frc.robot.subsystems.drivetrain;
 
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+// for specific code structure examples, refer to
+// https://github.com/BananasAmIRite/offseason-2025/tree/code-structure/src/main/java/frc/robot/subsystems
+@Logged
 public class Drivetrain extends SubsystemBase {}
--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -1,6 +1,10 @@
 /* (C) Robolancers 2025 */
 package frc.robot.subsystems.vision;
 
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+// for specific code structure examples, refer to
+// https://github.com/BananasAmIRite/offseason-2025/tree/code-structure/src/main/java/frc/robot/subsystems
+@Logged
 public class Vision extends SubsystemBase {}
