1351a90d76e3c50c197950498f4a87772de671ca
BananasAmIRite <jasony1230@gmail.com>
2025-01-17 16:02:19 -0500

    fix: added comments for procedures for general logging

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -6,5 +6,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 // for specific code structure examples, refer to
 // https://github.com/BananasAmIRite/offseason-2025/tree/code-structure/src/main/java/frc/robot/subsystems
+
+// `@Logged` must be placed in front of every class you wish to log
+// for more info about logging, visit:
+// https://docs.wpilib.org/en/stable/docs/software/telemetry/robot-telemetry-with-annotations.html#new-for-2025-robot-telemetry-with-annotations-java-only
 @Logged
 public class Vision extends SubsystemBase {}
